4cdd810b36a596f13e3cf706bc4a0c98e0bbce1d
Shevelev <shevelov@gmail.com>
2022-04-18 06:58:43 +0400

    top and bottom pipes, pipes (holes) generator with random difficulty, spped up every tenth hole

--- game.js
+++ game.js
@@ -28,6 +28,7 @@ function preload() {
 
     this.load.image('image_back', 'assets/back.png');
     this.load.image('image_pipe', 'assets/pipe.png');
+    this.load.image('image_pipe_bottom', 'assets/pipe_bottom.png');
 
     this.load.spritesheet('spritesheet_bird',
         'assets/bird.png',
@@ -45,12 +46,17 @@ const BIRD_POS_Y = (GAME_HEIGHT / 2) - 100;
 // starting pipes horizontal shift speed per update call
 const ACCELERATION = 0.003;
 let gameSpeed = GAME_WIDTH * ACCELERATION;
-const GRAVITY = 300;
+const GRAVITY = 400;
 const GRAVITY_ZERO = 0;
-const FLAP_VELOCITY = 350;
+const FLAP_VELOCITY = 250;
 
-const PIPE_GAP_STEP = GAME_WIDTH / 5;
-const PIPE_WIDTH = 60; // TODO change to Image.width
+// TODO: can I get sizes from image itself?
+const PIPE_WIDTH = 60;
+const PIPE_HEIGHT = 530;
+
+const PIPE_NUM = 3.2
+const PIPE_GAP_STEP = GAME_WIDTH / PIPE_NUM;
+const HOLE_GAP_STEP = GAME_HEIGHT / PIPE_NUM;
 
 const MESSAGE_STYLE = { 
     
@@ -70,23 +76,26 @@ const SCORE_STYLE = {
 }
 const SCORE_MESSAGE_TEXT = "Score: ";
 
-let pipes,
+let pipesTop, pipesBottom,
     bird,
     spacebarKey, escapeKey,
     pauseMessage, finishMessage, scoreMessage,
-    counter = 0;
+    counter = 0,
+    counterSpeedUp = 1;
 
 
 function create() {
 
     this.add.image(0, 0, 'image_back').setOrigin(0, 0);
 
-    pipes = this.physics.add.staticGroup();
+    pipesTop = this.physics.add.staticGroup();
+    pipesBottom = this.physics.add.staticGroup();
 
     makeBird(this.physics, this.anims);
 
-    this.physics.add.collider(bird, pipes, birdCollide, null, this);
-    this.physics.add.overlap(bird, pipes, birdOverlap, null, this);
+    //this.physics.add.collider(bird, pipes, birdCollide, null, this);
+    this.physics.add.overlap(bird, pipesTop, birdOverlap, null, this);
+    this.physics.add.overlap(bird, pipesBottom, birdOverlap, null, this);
 
     spacebarKey =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     escapeKey =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
@@ -120,14 +129,13 @@ function makeBird(physics, anims) {
         repeat: 1
     });
 
-    //bird.body.setGravityY(GRAVITY);
 }
 
 
-function birdCollide() {
+// function birdCollide() {
 
-    //console.log("Collide!");
-}
+//     //console.log("Collide!");
+// }
 
 
 function gameRestart() {
@@ -139,6 +147,8 @@ function gameRestart() {
     counter = 0;
     scoreMessage.setText(SCORE_MESSAGE_TEXT + counter);
     scoreMessage.setVisible(true);
+
+    counterSpeedUp = 1;
 
     bird.setCollideWorldBounds(true);
     bird.setPosition(BIRD_POS_X, BIRD_POS_Y);
@@ -160,7 +170,6 @@ function birdOverlap() {
     bird.setVelocityY(FLAP_VELOCITY);
     bird.setCollideWorldBounds(false);
 
-    // TODO: show results
     scoreMessage.setVisible(false);
 
     finishMessage.setText("Your score: " + counter + "\n" + FINISH_MESSAGE_TEXT);
@@ -192,31 +201,50 @@ function update() {
 
     pollKeyboard();
 
-    let allPipes = pipes.getChildren();
-
     if(gameState === GAME_STATE_PLAY) {
 
-        for(let i = 0; i < allPipes.length; i++) {
+        let allPipesTop = pipesTop.getChildren();
+        let allPipesBottom = pipesBottom.getChildren();
+
+        for(let i = 0; i < allPipesTop.length; i++) {
 
             // moving pipes RTL
-            allPipes[i].x -= gameSpeed;
-            allPipes[i].refreshBody();
+            allPipesTop[i].x -= gameSpeed;
+            allPipesBottom[i].x -= gameSpeed;
 
-            if(allPipes[i].x <= BIRD_POS_X && allPipes[i].x >= BIRD_POS_X - gameSpeed - 1) {
+            allPipesTop[i].refreshBody();
+            allPipesBottom[i].refreshBody();
+
+            // count the pipes pair that the bird has passed through
+            if(allPipesTop[i].x <= BIRD_POS_X && allPipesTop[i].x >= BIRD_POS_X - gameSpeed - 1) {
 
                 counter++;
                 scoreMessage.setText(SCORE_MESSAGE_TEXT + counter);
             }
+
+            // sppeding up every 10 gates
+            if(counter > counterSpeedUp && counter % 10 === 0) {
+                
+                gameSpeed += gameSpeed * ACCELERATION * 50;
+
+                //console.log(gameSpeed);
+                counterSpeedUp = counter;
+            }
             
-            if(i === allPipes.length - 1 && allPipes[i].x < GAME_WIDTH - PIPE_GAP_STEP)
+            // create new pair of pipes,
+            // if the last (newest) pair has moved more than PIPE_GAP_STEP
+            if(i === allPipesTop.length - 1 && allPipesTop[i].x < GAME_WIDTH - PIPE_GAP_STEP)
                 addPipes();
         }
 
-        //console.log(allPipes.length);
+        //console.log(allPipesTop.length);
         
-        // destroys first pipe if it moved out of screen
-        if(allPipes.length !== 0 && allPipes[0].x < -PIPE_WIDTH / 2)
-            pipes.remove(allPipes[0], true, true);
+        // destroys first pair of pipes if it moved out of screen
+        if(allPipesTop.length !== 0 && allPipesTop[0].x < -PIPE_WIDTH / 2) {
+            
+            pipesTop.remove(allPipesTop[0], true, true);
+            pipesBottom.remove(allPipesBottom[0], true, true);
+        }
     }
 }
 
@@ -224,11 +252,13 @@ function update() {
 // on game finish
 function clearPipes() {
 
-    let allPipes = pipes.getChildren();
+    let allPipesTop = pipesTop.getChildren();
+    let allPipesBottom = pipesBottom.getChildren();
 
-    while(allPipes.length > 0) {
+    while(allPipesTop.length > 0) {
 
-        pipes.remove(allPipes[0], true, true);
+        pipesTop.remove(allPipesTop[0], true, true);
+        pipesBottom.remove(allPipesBottom[0], true, true);
 
         //allPipes = pipes.getChildren();
     }
@@ -250,8 +280,6 @@ function birdFlap() {
 function setPause() {
 
     //console.log(" * Pause");
-
-    // TODO: output message "Press SPACE to play"
 
     gameState = GAME_STATE_PAUSE;
     //GAME.scene.pause('default');
@@ -275,13 +303,66 @@ function setPlay() {
 }
 
 
-// each member is a pair
-const PIPE_CONFIG = [
-    [0, PIPE_GAP_STEP]
+// vertical hole size
+const HOLE_SIZE = [
+    HOLE_GAP_STEP * PIPE_NUM,
+    HOLE_GAP_STEP * PIPE_NUM / 1.5,
+    HOLE_GAP_STEP * PIPE_NUM / 2,
+    HOLE_GAP_STEP * PIPE_NUM / 3,
+    HOLE_GAP_STEP * PIPE_NUM / 4
+];
+
+// probability of diff hole sizes
+const HOLE_DIFFICULTY = [
+    0,
+    1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3,
+    4, 4, 4
 ];
 
 
+let prevHoleCenter = GAME_HEIGHT / 2;
+
 function addPipes() {
 
-    pipes.create(GAME_WIDTH + PIPE_WIDTH / 2, 0, 'image_pipe');
+    // clamps center of the hole to fit it whole on the screen
+    // while making it's not to different in position from previous hole
+    const findCenter = function(holeWidth) {
+
+        const RAND_Y = Math.random() * GAME_HEIGHT;
+
+        let center = Math.min(RAND_Y, GAME_HEIGHT - holeWidth / 2, prevHoleCenter + HOLE_GAP_STEP);
+        center = Math.max(center, holeWidth / 2, prevHoleCenter - HOLE_GAP_STEP);
+
+        return center;
+    }
+
+    // HOLE_SIZE.forEach(size => console.log(size));
+    
+    // let a = 20;
+    
+    // while(a > 0) {
+
+    //     let randWidth = HOLE_SIZE[HOLE_DIFFICULTY[Math.floor(Math.random() * HOLE_DIFFICULTY.length)]];
+    
+    //     //console.log(randWidth);
+
+    //     let holeWidth = HOLE_SIZE[HOLE_DIFFICULTY[Math.floor(Math.random() * HOLE_DIFFICULTY.length)]];
+
+    //     console.log(holeWidth, findCenter(Math.random() * GAME_HEIGHT, holeWidth));
+
+    //     a--;
+    // }
+
+    let holeWidth = HOLE_SIZE[HOLE_DIFFICULTY[Math.floor(Math.random() * HOLE_DIFFICULTY.length)]];
+
+    let holeCenter = findCenter(holeWidth);
+
+    //console.log(holeCenter, " / ", holeWidth);
+    prevHoleCenter = holeCenter;
+    
+    pipesTop.create(GAME_WIDTH + PIPE_WIDTH / 2, holeCenter - (holeWidth + PIPE_HEIGHT) / 2, 'image_pipe');
+    
+    pipesBottom.create(GAME_WIDTH + PIPE_WIDTH / 2, holeCenter + (holeWidth + PIPE_HEIGHT) / 2, 'image_pipe_bottom');
 }
